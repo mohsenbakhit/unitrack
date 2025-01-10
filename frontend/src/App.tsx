3ca6604c448027fcb96bde7ad7514e15063d9e60
Mohsen Bakhit <bakhitmohsen@proton.me>
2025-01-09 21:37:19 -0500

    Add login page, cookies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './pages/home';
 import LoginPage from './pages/LoginPage/login';
+import RegisterPage from './pages/RegisterPage/register';
+import './index.css';
 function App() {
   return (
-    <div>
+    <CookiesProvider>
       <Navbar />
       <Routes>
-         <Route path="/" element={<HomePage />}/>
-        <Route path="/about" element={<h1>About Page</h1>} />
+        <Route path="/" element={<HomePage />}/>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
-    </div>
+    </CookiesProvider>
   );
 }
 
